7064914b6329726735fa107b2d56660439c0700a
Priscila Melo <melo.priscilatais@gmail.com>
2022-10-24 20:13:53 -0300

    sixth commit

--- src/data/tasksList.ts
+++ src/data/tasksList.ts
@@ -1,7 +1,7 @@
 import { listaTasks, Tasks } from "../models/tasks";
 import { User } from "../models/user";
 
-export const tasksList: listaTasks = [
+export const tasksList = [
   new Tasks("teste 01", "teste 01"),
   new Tasks("teste 02", "teste 02"),
 ];
