b48c14bc00648832754e5fa152819c12c1d552aa
Priscila Melo <melo.priscilatais@gmail.com>
2022-10-24 20:08:40 -0300

    fifth commit

--- src/README.md
+++ src/README.md
@@ -5,3 +5,7 @@ API de Tasks
 # comando para rodar api
 
 npm run dev
+
+# recolocar script package.json
+
+// "start": "json-server --watch ./src/data/db.json --port 3333",
--- src/data/tasksList.ts
+++ src/data/tasksList.ts
@@ -1,12 +1,12 @@
 import { listaTasks, Tasks } from "../models/tasks";
 import { User } from "../models/user";
 
-export const tasksList: listaTasks = [
+export const tasksList = [
   new Tasks("teste 01", "teste 01"),
   new Tasks("teste 02", "teste 02"),
 ];
 
 export const userList = [
-  new User("ricardo@teste.com", "0987!", "0987!", tasksList),
-  new User("teste@teste.com", "1234@", "1234@", tasksList),
+  new User("ricardo@teste.com", "0987!", "0987!"),
+  new User("teste@teste.com", "1234@", "1234@"),
 ];
