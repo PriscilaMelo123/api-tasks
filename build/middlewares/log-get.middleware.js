4a862b38b99ebefd19e87c803c7f4351eec16df4
Priscila Melo <melo.priscilatais@gmail.com>
2022-11-02 13:56:34 -0300

    Thirteenth commit

--- build/middlewares/log-get.middleware.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.logGetMiddleware = void 0;
-const logGetMiddleware = (req, res, next) => {
-    console.log("LISTAR TODAS TASKS");
-    next();
-};
-exports.logGetMiddleware = logGetMiddleware;
--- build/middlewares/log.middleware.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.logMiddleware = void 0;
-const logMiddleware = (req, res, next) => {
-    console.log("Passou aqui no middleware!");
-    next();
-};
-exports.logMiddleware = logMiddleware;
